6fdeeba70b5d1b692cf741294f28fae2d3beb2b1
Filipe Varjão <varjaofilipe@gmail.com>
2012-02-08 18:15:22 -0300

    Requisição .json

--- mainwindow.py
+++ mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'mainwindow.ui'
 #
-# Created: Wed Feb  8 12:45:32 2012
+# Created: Wed Feb  8 16:45:28 2012
 #      by: PyQt4 UI code generator 4.7.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,11 +12,11 @@ from PyQt4 import QtCore, QtGui
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(523, 384)
+        MainWindow.resize(794, 570)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayoutWidget = QtGui.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 381, 311))
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 0, 681, 511))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
         self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
         self.verticalLayout.setObjectName("verticalLayout")
@@ -24,22 +24,22 @@ class Ui_MainWindow(object):
         self.listWidget.setObjectName("listWidget")
         self.verticalLayout.addWidget(self.listWidget)
         self.coligado = QtGui.QPushButton(self.centralwidget)
-        self.coligado.setGeometry(QtCore.QRect(410, 20, 93, 27))
+        self.coligado.setGeometry(QtCore.QRect(700, 30, 93, 71))
         self.coligado.setObjectName("coligado")
         self.curso = QtGui.QPushButton(self.centralwidget)
-        self.curso.setGeometry(QtCore.QRect(410, 70, 93, 27))
+        self.curso.setGeometry(QtCore.QRect(700, 110, 93, 71))
         self.curso.setObjectName("curso")
         self.disciplina = QtGui.QPushButton(self.centralwidget)
-        self.disciplina.setGeometry(QtCore.QRect(410, 120, 93, 27))
+        self.disciplina.setGeometry(QtCore.QRect(700, 190, 93, 71))
         self.disciplina.setObjectName("disciplina")
         self.turma = QtGui.QPushButton(self.centralwidget)
-        self.turma.setGeometry(QtCore.QRect(410, 170, 93, 27))
+        self.turma.setGeometry(QtCore.QRect(700, 270, 93, 71))
         self.turma.setObjectName("turma")
         self.novo = QtGui.QPushButton(self.centralwidget)
-        self.novo.setGeometry(QtCore.QRect(140, 320, 93, 27))
+        self.novo.setGeometry(QtCore.QRect(470, 520, 93, 27))
         self.novo.setObjectName("novo")
         self.editar = QtGui.QPushButton(self.centralwidget)
-        self.editar.setGeometry(QtCore.QRect(250, 320, 93, 27))
+        self.editar.setGeometry(QtCore.QRect(560, 520, 93, 27))
         self.editar.setObjectName("editar")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtGui.QStatusBar(MainWindow)
--- test.py
+++ test.py
@@ -5,6 +5,16 @@ import sys
 from PyQt4 import QtCore, QtGui
 from mainwindow import Ui_MainWindow
 
+def f():
+        import urllib
+#        full = urllib.urlopen("http://localhost:3000/posts.json")#"https://www.googleapis.com/plus/v1/people/104180219523854245733.json")
+#        contents = full.read()
+#        return contents
+        url = 'http://localhost:3000/posts.json'
+        f = urllib.urlopen(url)
+        d = f.read()
+        return d
+
 class TestWindow(QtGui.QMainWindow, Ui_MainWindow):
         def __init__(self):
                 QtGui.QMainWindow.__init__(self)
@@ -12,11 +22,33 @@ class TestWindow(QtGui.QMainWindow, Ui_MainWindow):
                 self.setupUi(self)
 
                 self.connect(self.coligado, QtCore.SIGNAL('clicked()'),self.related)
+                self.connect(self.curso, QtCore.SIGNAL('clicked()'),self.course)
+                self.connect(self.disciplina, QtCore.SIGNAL('clicked()'),self.discipline)
+                self.connect(self.turma, QtCore.SIGNAL('clicked()'),self.gang)
+                self.connect(self.novo, QtCore.SIGNAL('clicked()'),self.new)
+                self.connect(self.editar, QtCore.SIGNAL('clicked()'),self.update)
 
 
         def related(self):
-                self.listWidget.addItem("Coligados tem que aparecer na janela")
+        
+                e = f()
+                self.listWidget.addItem(e)
 
+        def course(self):
+                self.listWidget.addItem("Curso")
+                
+        def discipline(self):
+                self.listWidget.addItem("Json das disciplinas")
+                
+        def gang(self):
+                self.listWidget.addItem("Gangs")
+        
+        def new(self):
+                self.listWidget.addItem("Criar metodo Novo, nova tela")
+                
+        def update(self):
+                self.listWidget.addItem("criar metodo update")
+                
 app = QtGui.QApplication(sys.argv)
 form = TestWindow()
 form.show()
